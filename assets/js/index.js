a707ce7cccfa8dc2237acb1c3e8fcdfde5a0f2b0
palminski <williambolls98@gmail.com>
2022-08-17 10:24:12 -0700

    fetched current information on weather

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,38 @@
+//API KEY FROM BCS: 5253d1895ec18b3c6485974e30c75532
 
-//Works but not the correct API (Json data missing UV index)
-const fetchWeatherInfoForecastAPI = function(cityName) {
+// this gets lat and lon coords
+const getWeather = function(cityName) {
     const API_KEY = "f5679ff18384584c4ebc83f9054ae558";
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+API_KEY+"&units=imperial").then(function(response){
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid="+API_KEY).then(function(response){
         response.json().then(function(data){
-            console.log(data);
+            
+            let lat = data[0].lat
+            let lon = data[0].lon
+            fetchWeatherInfo(lat,lon)
         })
     })
 }
 
-//This is the API that was linked on the project page. this results in a 401 error
-const fetchWeatherInfo = function(cityName) {
-    const API_KEY = "f5679ff18384584c4ebc83f9054ae558";
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid="+API_KEY).then(function(response){
+//This gets weather data
+const fetchWeatherInfo = function(lat,lon) {
+    const API_KEY = "5253d1895ec18b3c6485974e30c75532";
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly,minutely&appid="+API_KEY+"&units=imperial").then(function(response){
         response.json().then(function(data){
-            console.log(data);
+            //Current Weather Data
+            let currentDate = data.current.dt;
+            let currentTemp = data.current.temp;
+            let currentWind = data.current.wind_speed;
+            let currentHumidity = data.current.humidity;
+            let currentUvi = data.current.uvi;
+
+            console.log(currentDate);
+            console.log("Temp "+currentTemp);
+            console.log("Wind "+ currentWind);
+            console.log("Humidity "+currentHumidity);
+            console.log("UVI "+currentUvi);
+
+            //Future Weather Data
+            for (let i = 0;)
         })
     })
 }
@@ -24,5 +42,5 @@ const fetchWeatherInfo = function(cityName) {
 //fetchWeatherInfoForecastAPI("Eugene");
 
 
-fetchWeatherInfo(); //curently resulting in 401 Unauthorised
+getWeather("Eugene"); //curently resulting in 401 Unauthorised
 
