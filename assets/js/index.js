b6eadf1a06f4f61de01d3e10ac8209f9614bc79c
palminski <williambolls98@gmail.com>
2022-08-17 21:31:04 -0700

    Error messages

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,81 +15,82 @@ const $searchForm = document.querySelector("#search-form");
 const $searchInput = document.querySelector("#search-input");
 const $historyButtonsContainer = document.querySelector("#history-buttons");
 
-let history=[];
-if (localStorage.getItem("history")){
+let history = [];
+if (localStorage.getItem("history")) {
     history = JSON.parse(localStorage.getItem("history"))
 }
 
 
 //When Search Form Submitted
-const formSubmitHandler = function(event) {
+const formSubmitHandler = function (event) {
     event.preventDefault();
     let searchTerm = $searchInput.value.trim();
     if (searchTerm) {
         getWeather(searchTerm);
-        $searchInput.value="";
+        $searchInput.value = "";
     }
-    else
-    {
+    else {
         alert("Please Enter a city name");
     }
 }
 
 //When a history button is clicked
-const buttonClickHandler = function(event) {
+const buttonClickHandler = function (event) {
     let location = event.target.textContent;
-    if (location){
+    if (location) {
         getWeather(location);
-        $searchInput.value="";
+        $searchInput.value = "";
     }
 }
 
-const updateHistory = function(searchTerm){
-    for (let i = 0; i < history.length ; i++){
-        if (history[i] === searchTerm){
-            history.splice(i,1);
+const updateHistory = function (searchTerm) {
+    for (let i = 0; i < history.length; i++) {
+        if (history[i] === searchTerm) {
+            history.splice(i, 1);
             i = 0;
         }
-        
+
     }
     history.unshift(searchTerm);
     while (history.length > 5) {
         history.pop();
         console.log(history);
     }
-    localStorage.setItem("history",JSON.stringify(history));
+    localStorage.setItem("history", JSON.stringify(history));
     updateHistoryButtons();
 }
 
-const updateHistoryButtons = function() {
-    
-    $historyButtonsContainer.innerHTML="";
+const updateHistoryButtons = function () {
+
+    $historyButtonsContainer.innerHTML = "";
     for (let i = 0; i < history.length; i++) {
         let $historyButton = document.createElement("button");
         $historyButton.classList = "btn btn-primary w-100 mb-2";
-        $historyButton.textContent= history[i];
+        $historyButton.textContent = history[i];
         $historyButtonsContainer.appendChild($historyButton);
     }
 }
 // this gets lat and lon coords
-const getWeather = function(cityName) {
+const getWeather = function (cityName) {
     const API_KEY = "f5679ff18384584c4ebc83f9054ae558";
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid="+API_KEY).then(function(response){
-        response.json().then(function(data){
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + API_KEY)
+    .then(response => response.json()) //parsing response
+        .then(function (data) {
             let location = data[0].name;
             let lat = data[0].lat;
             let lon = data[0].lon;
             updateHistory(location);
-            fetchWeatherInfo(lat,lon,location);
-        })
+            fetchWeatherInfo(lat, lon, location);
+        }).catch(function () {
+            alert("ERROR Unable to recieve data for location");
     })
 }
 
 //This gets weather data
-const fetchWeatherInfo = function(lat,lon,location) {
+const fetchWeatherInfo = function (lat, lon, location) {
     const API_KEY = "5253d1895ec18b3c6485974e30c75532";
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly,minutely&appid="+API_KEY+"&units=imperial").then(function(response){
-        response.json().then(function(data){
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=" + API_KEY + "&units=imperial").then(function (response) {
+        response.json().then(function (data) {
             //Today's Weather Data
             let currentDate = luxon.DateTime.fromSeconds(data.daily[0].dt).toLocaleString();
             let currentIcon = data.daily[0].weather[0].icon;
@@ -100,12 +101,12 @@ const fetchWeatherInfo = function(lat,lon,location) {
 
             $city.textContent = location;
             $todaysDate.textContent = currentDate;
-            $todaysIcon.setAttribute("src",`https://openweathermap.org/img/wn/${currentIcon}@2x.png`)
+            $todaysIcon.setAttribute("src", `https://openweathermap.org/img/wn/${currentIcon}@2x.png`)
             $todaysTemp.textContent = currentTemp;
             $todaysWind.textContent = currentWind;
-            $todaysHumidity.textContent =currentHumidity;
+            $todaysHumidity.textContent = currentHumidity;
             $todaysUVI.textContent = currentUvi;
-            
+
             if (currentUvi < 3) {
                 $todaysUVI.classList = "text-success";
             }
@@ -118,14 +119,14 @@ const fetchWeatherInfo = function(lat,lon,location) {
 
             //Forecast Weather Data
             $forecastCardContainer.innerHTML = ("");
-    
+
             for (let i = 1; i < 6; i++) {
                 let date = luxon.DateTime.fromSeconds(data.daily[i].dt).toLocaleString();
                 let icon = data.daily[i].weather[0].icon;
                 let temp = data.daily[i].temp.day;
                 let wind = data.daily[i].wind_speed;
                 let humidity = data.daily[i].humidity;
-                
+
                 let $forecastCard = document.createElement("div");
                 let $date = document.createElement("h3");
                 let $icon = document.createElement("img");
@@ -135,11 +136,11 @@ const fetchWeatherInfo = function(lat,lon,location) {
 
                 $forecastCard.classList = "forecast-card col";
                 $date.textContent = (date);
-                $icon.setAttribute("src",`https://openweathermap.org/img/wn/${icon}@2x.png`);
-                $icon.setAttribute("alt",`weather icon`);
-                $temp.textContent = ("temp: "+temp);
-                $wind.textContent = ("wind: "+wind);
-                $humidity.textContent = ("humidity: "+humidity);
+                $icon.setAttribute("src", `https://openweathermap.org/img/wn/${icon}@2x.png`);
+                $icon.setAttribute("alt", `weather icon`);
+                $temp.textContent = ("temp: " + temp);
+                $wind.textContent = ("wind: " + wind);
+                $humidity.textContent = ("humidity: " + humidity);
 
                 $forecastCard.appendChild($date);
                 $forecastCard.appendChild($icon);
@@ -150,10 +151,12 @@ const fetchWeatherInfo = function(lat,lon,location) {
                 $forecastCardContainer.appendChild($forecastCard);
             }
         })
-    })
+    }).catch(function () {
+        alert("unable to connect");
+    });
 }
 
 updateHistoryButtons();
-$searchForm.addEventListener("submit",formSubmitHandler);
-$historyButtonsContainer.addEventListener("click",buttonClickHandler);
+$searchForm.addEventListener("submit", formSubmitHandler);
+$historyButtonsContainer.addEventListener("click", buttonClickHandler);
 
